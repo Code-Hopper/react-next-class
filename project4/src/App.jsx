4603d4461495de7f4af0dfcdb45802cabeea24c0
code-hopper <ameykhondekar01@gmail.com>
2025-05-22 11:29:10 +0530

    conditional rendering

--- project4/src/App.jsx
+++ project4/src/App.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
 
+import Home from "./components/page/Home"
+
+import { students } from './data.js'
+
 const App = () => {
+
+  let data = 20
+
   return (
-    <div>
-      <h1 className='text-3xl text-color3 font-bold'>hello react</h1>
-    </div>
+    <>
+      {/* <Home /> */}
+
+      {
+        data > 10 ? <h1>Greater Than 10</h1> : <h1>Less than 10</h1>
+      }
+
+      <h1>the value of data {data}</h1>
+
+      <table className='table'>
+        <thead className='table-header-group'>
+          <th>Name</th>
+          <th>class</th>
+        </thead>
+        <tbody>
+          {
+            students.map((student, index) => {
+              return (
+                <tr key={index}>
+                  <td>{student.name}</td>
+                  <td>{student.class}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+
+    </>
   )
 }
 
 export default App
+
+
+// condition ? true block : flase block
---
+++ project4/src/components/page/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Header } from '../includes/Header'
+import Footer from '../includes/Footer'
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    )
+}
+
+export default Home
